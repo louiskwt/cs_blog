c616010a42efa207e7554dcec085e941e32e6c46
Louis Tsang <kawingt2@gmail.com>
2022-11-01 22:52:09 +0800

    created model manager

--- myblog/blogs/models.py
+++ myblog/blogs/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
@@ -14,6 +18,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ["-published_at"]
