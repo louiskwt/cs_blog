dd8612619a3c52c4e2a2827f17cbe061220a059b
Louis Tsang <kawingt2@gmail.com>
2022-11-03 22:57:34 +0800

    implemented blog detail page

--- myblog/blogs/urls.py
+++ myblog/blogs/urls.py
@@ -6,4 +6,5 @@ app_name = "blogs"
 urlpatterns = [
     # post views
     path("", views.post_list, name="home"),
+    path('<int:id>', views.post_detail, name="post_detail"),
 ]
--- myblog/blogs/views.py
+++ myblog/blogs/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'home.html', {"posts": posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, 'blog/detail.html', {'post': post})
