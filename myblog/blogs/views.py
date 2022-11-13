f50e61122f0879c89467bf06f7415dfb23fe4c59
Louis Tsang <kawingt2@gmail.com>
2022-11-13 22:44:14 +0800

    implemented pagination

--- myblog/blogs/views.py
+++ myblog/blogs/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage
 
 def post_list(request):
-    posts = Post.published.all()
+
+    post_list = Post.published.all()
+
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # if page number is out of range
+        posts = paginator.page(paginator.num_pages)
+        
     return render(request, 'home.html', {"posts": posts})
 
 def post_detail(request, year, month, day, post):
